7373b32e809b742fe8767819ca859ffc17aecb56
Lasse Diercks <github@lassediercks.de>
2017-12-14 19:28:39 +0100

    Add optional filename
    so you can have a proper pdf name

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-const puppeteer = require('puppeteer');
-const express = require('express')
-const app = express()
-const validUrl = require('valid-url');
+const puppeteer = require("puppeteer");
+const express = require("express");
+const app = express();
+const validUrl = require("valid-url");
 const port = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   if (!req.params.url) {
-    res.send('url fehlt lol!');
+    res.send("url is missing lol!");
     res.end();
   }
 });
 
-app.get('/:url', async (req, res) => {
-  if(validUrl.isUri(req.params.url)){
+app.get("/:url", async (req, res) => {
+  if (validUrl.isUri(req.params.url)) {
     res.contentType("application/pdf");
 
-    const browser = await puppeteer.launch({args: ['--no-sandbox','--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
     const page = await browser.newPage();
 
-    await page.goto(req.params.url, {waitUntil: 'networkidle'});
-    const data = await page.pdf({format: 'A4'});
+    await page.goto(req.params.url, { waitUntil: "networkidle" });
+    const data = await page.pdf({
+      format: "A4",
+      filename: req.params.url + ".pdf"
+    });
     browser.close();
     res.send(data);
     res.end();
   } else {
-    res.send(req.params.url + 'is not a valid url');
+    res.send(req.params.url + "is not a valid url");
   }
-
 });
 
+app.get("/:url/:filename", async (req, res) => {
+  if (validUrl.isUri(req.params.url)) {
+    res.contentType("application/pdf");
 
-app.listen(port, function () {
-  console.log('server running')
-})
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    const page = await browser.newPage();
+
+    await page.goto(req.params.url, { waitUntil: "networkidle" });
+    const data = await page.pdf({
+      format: "A4",
+      filename: req.params.filename + ".pdf"
+    });
+    browser.close();
+    res.send(data);
+    res.end();
+  } else {
+    res.send(req.params.url + "is not a valid url");
+  }
+});
+
+app.listen(port, function() {
+  console.log("server running");
+});
