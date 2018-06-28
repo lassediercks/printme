532e95802e024ec80846078378514d538b015d7b
Lasse Diercks <github@lassediercks.de>
2018-06-28 22:54:33 +0200

    fix: try to avoid memory issues

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ setInterval(() => {
 async function createPdf(url, res) {
   if (validUrl.isUri(url)) {
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+      ],
     });
 
     const page = await browser.newPage();
