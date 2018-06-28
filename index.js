e5df010f4f531a5408dad2868489c2062cbf3dfc
Lasse Diercks <github@lassediercks.de>
2018-06-28 18:05:44 +0200

    feat: introduce post request acceptance, remove filename api

--- index.js
+++ index.js
@@ -1,68 +1,51 @@
-const puppeteer = require("puppeteer");
-const express = require("express");
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+const express = require('express');
+const validUrl = require('valid-url');
+const cors = require('cors');
+const http = require('http');
+
 const app = express();
-const validUrl = require("valid-url");
 const port = process.env.PORT || 3000;
 
-var http = require("http");
+app.use(bodyParser.json());
 
-app.set("port", process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000);
 
-setInterval(function() {
-  http.get("http://printmenow.herokuapp.com");
+setInterval(() => {
+  http.get('http://printmenow.herokuapp.com');
 }, 300000);
 
-app.get("/", async (req, res) => {
-  if (!req.params.url) {
-    res.send("url is missing lol!");
-    res.end();
-  }
-});
-
-app.get("/:url", async (req, res) => {
-  if (validUrl.isUri(req.params.url)) {
-    res.contentType("application/pdf");
-
+async function createPdf(url, res) {
+  if (validUrl.isUri(url)) {
     const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
-    const page = await browser.newPage();
 
-    await page.goto(req.params.url, { waitUntil: "networkidle" });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle' });
+
     const data = await page.pdf({
-      format: "A4",
-      filename: req.params.url + ".pdf"
+      format: 'A4',
+      filename: `${url}.pdf`,
     });
-    browser.close();
+
+    res.contentType('application/pdf');
     res.send(data);
     res.end();
   } else {
-    res.send(req.params.url + "is not a valid url");
+    res.send(`${url} is not a valid url`);
   }
+}
+
+app.post('/endpoint', cors(), (req, res) => {
+  createPdf(req.body.url, res);
 });
 
-app.get("/:url/:filename", async (req, res) => {
-  if (validUrl.isUri(req.params.url)) {
-    res.contentType("application/pdf");
-
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
-    const page = await browser.newPage();
-
-    await page.goto(req.params.url, { waitUntil: "networkidle" });
-    const data = await page.pdf({
-      format: "A4",
-      filename: req.params.filename + ".pdf"
-    });
-    browser.close();
-    res.send(data);
-    res.end();
-  } else {
-    res.send(req.params.url + "is not a valid url");
-  }
+app.get('/:url', async (req, res) => {
+  createPdf(req.params.url, res);
 });
 
 app.listen(port, function() {
-  console.log("server running");
+  console.log('server running');
 });
