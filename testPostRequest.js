c9f90e7068fc4bb7a20229c1bde6fef1cff9e6af
Lasse Diercks <github@lassediercks.de>
2018-06-29 15:44:18 +0200

    update test post request

--- testPostRequest.js
+++ testPostRequest.js
@@ -1,10 +1,29 @@
+function createPdf(body) {
+  const blob = new Blob([body], { type: 'application/pdf' });
+  const data = window.URL.createObjectURL(blob);
+
+  const link = document.createElement('a');
+
+  console.log(typeof body);
+  // The actual download
+  // var filename = "blah.pdf";
+  link.href = data;
+  console.log(link);
+  document.body.appendChild(link);
+  link.click();
+}
+
+url = 'http://localhost:3000/endpoint';
+
 options = {
   method: 'POST',
-  body: JSON.stringify({ url: 'https://www.google.com' }),
+  body: JSON.stringify({ url: 'http://google.com' }),
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
 };
 
-fetch('http://localhost:3000/endpoint', options);
+fetch(url, options)
+  .then((response) => response.arrayBuffer())
+  .then(createPdf);
